11d2eb1cda3f8201dc3bef4e7f8f5e1722fa79a2
DEMPSEY Adam <amd616@quser11.quest.it.northwestern.edu>
2017-08-29 18:40:53 -0500

    Updated status.

--- pyutils/status.py
+++ pyutils/status.py
@@ -11,7 +11,14 @@ dat[:,1] /= 2
 fld = Zavg(dat=dat)
 plt.ion()
 fld.tss()
-fld.summary(average=True,window=30)
+if fld.nt > 50:
+    fld.summary(average=True,window=30)
+elif fld.nt > 100:
+    fld.summary(average=True,window=70)
+elif fld.nt > 130:
+    fld.summary(average=True,window=100)
+else:
+    fld.summary(average=False)
 plt.show(block=True)
 
 
